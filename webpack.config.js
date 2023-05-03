8828f06297521f0a622458f445ad0e3957ab6885
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-05-03 08:29:07 -0600

    Update webapp title

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'TITLE',
+      title: '2Dwist',
       filename: 'index.html',
       template: 'src/index.html',
       favicon: 'src/assets/favicon.jpg',
