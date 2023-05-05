f1abed9bcc6bf45026c384953b446827e17ffee4
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-05-04 20:33:04 -0600

    Remove hard-coded list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/main.css';
+import TaskList from './modules/taskListClass.js';
+/* import renderTasks from './modules/renders.js'; */
 
 const tasksContainer = document.querySelector('.tasks-container');
 
@@ -31,18 +33,48 @@ const listOfTasks = [
 ];
 
 const sortTasks = (listOfTasks) => listOfTasks.sort((a, b) => a.index - b.index);
+const tasks = new TaskList();
 
+const tasksContainer = document.querySelector('.tasks-container');
 const renderTasks = (listOfTasks) => {
-  sortTasks(listOfTasks);
   tasksContainer.innerHTML = `
     ${listOfTasks.map((task) => `
       <li class="task dflex">
         <input type="checkbox" name="task-status" id="${task.id}-status">
+      <li class="task dflex"  id="task-${task.id}">
+        <input type="checkbox" name="task-status">
         ${task.description}
         <i class="fa-solid fa-ellipsis-vertical task-more"></i>
+        <div class="icon-swapper dflex">
+          <i class="fa-solid fa-ellipsis-vertical task-more"></i>
+          <i class="fa-solid fa-eraser task-delete"></i>
       </li>
     `).join('')
+      `).join('')
 }`;
+
+  /* EVENT HANDLERS */
+  const refreshIcon = document.querySelector('.refresh');
+  refreshIcon.addEventListener('click', () => {
+    window.location.reload();
+  });
+
+  const deleteIcon = document.querySelectorAll('.task-delete');
+  deleteIcon.forEach((icon) => {
+    icon.addEventListener('click', (e) => {
+      e.preventDefault();
+      const taskID = e.target.parentNode.parentNode.id.replace('task-', '');
+      tasks.removeTask(taskID);
+      renderTasks(tasks.getTasks());
+    });
+  });
 };
 
-renderTasks(listOfTasks);
+renderTasks(listOfTasks);dataEntryForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const entryField = document.querySelector('.task-entry');
+  tasks.addTask(entryField.value);
+  entryField.value = '';
+  renderTasks(tasks.getTasks());
+});
+renderTasks(tasks.getTasks());
