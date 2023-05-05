c65a41ed23fae8dc2b30be84fe2bc0069ce0d464
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-05-05 10:49:48 -0600

    Add logic for checkbox to update task status
    Add guiding comments to the code
    Remove commented code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles/main.css';
 import TaskList from './modules/taskListClass.js';
-/* import renderTasks from './modules/renders.js'; */
 
 const tasks = new TaskList();
 
@@ -9,8 +8,8 @@ const renderTasks = (listOfTasks) => {
   tasksContainer.innerHTML = `
     ${listOfTasks.map((task) => `
       <li class="task dflex"  id="task-${task.id}">
-        <input type="checkbox" name="task-status" ${task.completed ? 'checked' : 'unchecked'}>
-        <span class="task-desc">${task.description}</span>
+        <input class="task-complete" type="checkbox" name="task-status" ${task.completed ? 'checked' : 'unchecked'}>
+        <span class="task-desc${task.completed ? ' crossed-out' : ''}">${task.description}</span>
         <div class="icon-swapper dflex">
           <i class="fa-solid fa-ellipsis-vertical task-more"></i>
           <i class="fa-solid fa-eraser task-delete"></i>
@@ -24,6 +23,7 @@ const renderTasks = (listOfTasks) => {
     window.location.reload();
   });
 
+  /* Edit description */
   const currentDescription = document.querySelectorAll('.task-desc');
 
   currentDescription.forEach((task) => {
@@ -32,8 +32,9 @@ const renderTasks = (listOfTasks) => {
       const span = e.target;
       span.classList.add('hidden');
 
+      /* get the task id */
       const taskToChange = span.parentNode.id.replace('task-', '');
-      const oldDescription = span.innerText;
+      const oldDescription = span.innerText; // allow to cancel edit without changes
 
       const input = document.createElement('input');
       const icons = span.parentNode.querySelector('.icon-swapper');
@@ -43,7 +44,7 @@ const renderTasks = (listOfTasks) => {
       span.parentNode.insertBefore(input, icons);
 
       input.addEventListener('focus', () => {
-        input.select();
+        input.select(); // easy edit, select the input current description to overwrite
       });
       input.focus();
 
@@ -51,6 +52,7 @@ const renderTasks = (listOfTasks) => {
         localStorage.setItem('newDesc', JSON.stringify(input.value));
       });
 
+      /* Save edit when lose focus */
       input.addEventListener('blur', () => {
         const newDescription = JSON.parse(localStorage.getItem('newDesc'));
         localStorage.removeItem('newDesc');
@@ -64,6 +66,7 @@ const renderTasks = (listOfTasks) => {
         renderTasks(tasks.getTasks());
       });
 
+      /* Saves edit when pressing enter key */
       input.addEventListener('keyup', (event) => {
         if (event.key === 'Enter') {
           const newDescription = JSON.parse(localStorage.getItem('newDesc'));
@@ -81,6 +84,7 @@ const renderTasks = (listOfTasks) => {
     });
   });
 
+  // Delete tasks //
   const deleteIcon = document.querySelectorAll('.task-delete');
   deleteIcon.forEach((icon) => {
     icon.addEventListener('click', (e) => {
@@ -90,8 +94,19 @@ const renderTasks = (listOfTasks) => {
       renderTasks(tasks.getTasks());
     });
   });
+
+  // Complete tasks //
+  const currentStatus = document.querySelectorAll('.task-complete');
+  currentStatus.forEach((chkBox) => {
+    chkBox.addEventListener('click', (e) => {
+      const taskID = e.target.parentNode.id.replace('task-', '');
+      tasks.updateStatus(taskID, chkBox.checked);
+      renderTasks(tasks.getTasks());
+    });
+  });
 };
 
+// Add new task //
 const dataEntryForm = document.querySelector('.inputField');
 dataEntryForm.addEventListener('submit', (e) => {
   e.preventDefault();
